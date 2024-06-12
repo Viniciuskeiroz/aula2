26ff667417352990f69a72c71b1afafa6d781fe8
ViniciusQueirozMarti <viniciusqlima00@gmail.com>
2024-06-12 08:03:05 -0300

    Na aula 2 de git, criado o segundo arquivo

---
+++ ddt.py
@@ -0,0 +1,11 @@
+print("Obrigado pssor pelo chocolate")
+print("É noize")
+
+
+n1 = int(input("Digite o primeiro numero")
+
+n2 = int(input("Digite o segundo numero")
+
+soma = n1 + n2
+
+print(" a soma é : " , soma)
